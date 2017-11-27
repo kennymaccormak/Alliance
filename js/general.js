fbc2defa19b3c74af9dc1bffa254cbd5ea71e98a
Volodymyr Chygura <volodymyr.chygura@gmail.com>
2017-11-27 18:04:43 +0200

    created header, title-block

---
+++ js/general.js
@@ -0,0 +1,12 @@
+$(function () {
+    $('.bg-slider-items').slick({
+        dots: false,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear',
+        autoplay: true,
+        autoplaySpeed: 5000,
+        arrows: false
+    });
+});
